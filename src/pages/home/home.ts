b813ef13e39a70810e99410a3a7370fbd9032397
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-03-14 00:43:16 +0100

    First commit
    Added buttons and request to the ESP8266 server to control the rebot

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+data: any;
+error: any
+url = 'http://192.168.1.27/gpio/';
+
+  
+  constructor(public navCtrl: NavController, public http: HttpClient) {
 
   }
+  
+  goLeft() {
+    
+    return this.http.get(this.url+"L").
+    subscribe(result => this.data = result
+    )};
+    
+    
+    goRight() {
+      
+      return this.http.get(this.url+"R").
+      subscribe(result => this.data = result
+      )}
 
+      goForward() {
+        
+        return this.http.get(this.url+"F").
+        subscribe(result => this.data = result
+        )}
+
+        goBackward() {
+          
+          return this.http.get(this.url+"B").
+          subscribe(result => this.data = result
+          )}
 }
+
