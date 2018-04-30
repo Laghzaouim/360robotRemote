26cce82bc5937ebe3d5f02c283ed7709793965cc
Mohammed La <mohammed.laghzaoui@outlook.com>
2018-05-01 01:59:38 +0200

    Old provider code commented and implemnting reconnect method

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController, Loading, AlertController, LoadingController, NavParams } from 'ionic-angular';
-import { RobotProvider, IResponse } from '../../providers/robot/robot';
+import { Component, OnInit } from '@angular/core';
+import { NavController, 
+  //Loading, 
+  AlertController, 
+  //LoadingController, 
+  NavParams } from 'ionic-angular';
+//import { RobotProvider, IResponse } from '../../providers/robot/robot';
 import {Paho} from 'ng2-mqtt/mqttws31';
 
 
@@ -8,28 +12,31 @@ import {Paho} from 'ng2-mqtt/mqttws31';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   client;
-
-data: IResponse;
-loading: Loading;
-
-status: String
- 
-  constructor(public navCtrl: NavController, public provider: RobotProvider, public navParams: NavParams, private alertCtrl: AlertController, private loadingCtrl: LoadingController) {
+  
+  //data: IResponse;
+  //loading: Loading;
+  
+  status: String
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
+  }
+  
+  ngOnInit(): void {
     this.client = new Paho.MQTT.Client('broker.hivemq.com', 8000, 'qwegdfgrty12345rgdgfr65564dgf');
     
     this.client.connect({onSuccess: this.onConnected.bind(this)});
     this.onMessage();
     this.onConnectionLost();
   }
-
+  
   onConnected() {
   
     console.log("Connected");
     this.client.subscribe("outTopicMoIoT");
-    this.sendMessage('1');
+    this.sendMessage('start360robot');
     //debugger
   }
 
@@ -55,7 +62,24 @@ status: String
     //debugger
     this.client.onConnectionLost = (responseObject: Object) => {
       console.log('Connection lost : ' + JSON.stringify(responseObject));
+      this.showError("Unable to connect to the Internet")
+      this.onReconnection()
     };
+  }
+
+  onReconnection(){
+    this.client.connect({onSuccess: this.onConnected.bind(this)});
+  }
+
+  showError(text) {
+    //this.loading.dismiss();
+ 
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
   // sendReq(input: String) {
@@ -75,24 +99,14 @@ status: String
   //   })
   // }
 
-  showLoading() {
-    this.loading = this.loadingCtrl.create({
-      content: 'Please wait...',
-      dismissOnPageChange: true
-    });
-    this.loading.present();
-  }
+  // showLoading() {
+  //   this.loading = this.loadingCtrl.create({
+  //     content: 'Please wait...',
+  //     dismissOnPageChange: true
+  //   });
+  //   this.loading.present();
+  // }
  
-  showError(text) {
-    //this.loading.dismiss();
- 
-    let alert = this.alertCtrl.create({
-      title: 'Fail',
-      subTitle: text,
-      buttons: ['OK']
-    });
-    alert.present();
-  }
 
       
 }
